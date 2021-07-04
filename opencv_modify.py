56b66b921424298352aa7b11c040aebb9f68299a
dany <kod3000@gmail.com>
2021-07-04 08:44:20 -0400

    ...

---
+++ opencv_modify.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+
+
+class OpenCvModify():
+
+	def giveBrightness(img,amount) :
+
+		
+		# Convert unsigned int to float
+		img = np.float32(img)
+
+		# Scale the values so that they lie between [0,1]
+		#image = image * scalingFactor
+
+		brightnessOffset = amount
+
+		# Add the offset for increasing brightness
+		return img + brightnessOffset
