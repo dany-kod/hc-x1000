fac571674694c741a13ab1f38f5041c39385a28e
dany <kod3000@gmail.com>
2021-07-05 22:59:47 -0400

    Update testNetiFaces.py

--- testNetiFaces.py
+++ testNetiFaces.py
@@ -2,6 +2,7 @@ from netifaces import interfaces, ifaddresses, AF_INET
 foundIp = 'localhost';
 for ifaceName in interfaces():
 	for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] ):
+		print(i['addr'])
 		if i['addr'] != 'No IP addr':
 			foundIp = i['addr']
 
