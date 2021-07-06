71a5cb01335d322804f56f4f8a4b28a9664da1d8
dany <kod3000@gmail.com>
2021-07-05 23:26:04 -0400

    Update testSystem.py

--- testSystem.py
+++ testSystem.py
@@ -1,7 +1,13 @@
 from local_ip import ipLocalSystem as ip
+from camera_discovery import cameraDiscovery as cam
 
 if __name__ == '__main__':
     # test the server..
     print("testing that local Ip can be found.")
     localIp = ip.main()
     print(localIp)
+
+
+    print("testing camera discover can be found.")
+    camera = cam.main()
+    print(camera)
