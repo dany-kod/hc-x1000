a8362fdf402b97f1b6c6e6e9fedcf3a383b30590
dany <kod3000@gmail.com>
2021-07-06 14:35:07 -0400

    Auto Commit

--- lib/test.py
+++ lib/test.py
@@ -1,8 +1,8 @@
-from lib.server import hcXSERVER as hc
+from lib.server import hcXSERVER
 # this will find our local systems ip address for us.
 class testSystem():
 	def main():
-		what = hc.getTracks();
+		what = hcXSERVER.getTracks();
 		if what is None :
 			return "failed"
 		print(what)
