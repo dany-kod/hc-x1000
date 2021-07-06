4361c4cb4065e438e64136adee1d69adb1dd15ad
dany <kod3000@gmail.com>
2021-07-05 23:27:10 -0400

    Update camera_discovery.py

--- camera_discovery.py
+++ camera_discovery.py
@@ -2,12 +2,12 @@ from netdisco.discovery import NetworkDiscovery
 # this will find our local systems ip address for us.
 class cameraDiscovery():
     def main():
-    hcx1000Address = "Unknown";
-    netdis = NetworkDiscovery()
-    netdis.scan()
-    for dev in netdis.discover():
-        for i in netdis.get_info(dev):
-            if i['name'].find("X1000") != -1 :
-                hcx1000Address = i['host']
-    netdis.stop()
-    return hcx1000Address
+        hcx1000Address = "Unknown";
+        netdis = NetworkDiscovery()
+        netdis.scan()
+        for dev in netdis.discover():
+            for i in netdis.get_info(dev):
+                if i['name'].find("X1000") != -1 :
+                    hcx1000Address = i['host']
+        netdis.stop()
+        return hcx1000Address
